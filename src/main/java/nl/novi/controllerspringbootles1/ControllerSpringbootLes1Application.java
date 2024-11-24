2a85a709233ecaa8735581bd8a4a782d722fcf62
Gitaccount821 <nealvanzomeren@gmail.com>
2024-11-24 15:47:30 +0100

    first commit

---
+++ src/main/java/nl/novi/controllerspringbootles1/ControllerSpringbootLes1Application.java
@@ -0,0 +1,13 @@
+package nl.novi.controllerspringbootles1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerSpringbootLes1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControllerSpringbootLes1Application.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/controllerspringbootles1/ControllerSpringbootLes1ApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.controllerspringbootles1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerSpringbootLes1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
